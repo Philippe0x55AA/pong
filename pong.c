27e8dc632e9052d2ce5074ab2d5fa37f61f066ce
Philippe0x55AA <47057838+Philippe0x55AA@users.noreply.github.com>
2021-05-23 19:04:19 +0200

    Update pong.c

--- pong.c
+++ pong.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+SDL_Rect rect1 = {10,350,30,150};
+SDL_Rect rect2 = {1160,350,30,150};
+SDL_Rect separator = {600,0,5,950};
+SDL_Rect ball = {600,350,20,20};
+
+
+void draw_rect(){
+
+  SDL_SetRenderDrawColor(renderer,255,255,255,255);
+  SDL_RenderFillRect(renderer,&rect1);
+  SDL_RenderFillRect(renderer,&rect2);
+  SDL_RenderFillRect(renderer,&separator);
+  SDL_RenderFillRect(renderer,&ball);
+
+}
 int main(){
-  
-  SDL_Window *window = NULL;
   int status = EXIT_FAILURE;
-  SDL_Renderer *renderer = NULL;
-  SDL_Rect rect1 = {10,350,30,150};
-  SDL_Rect rect2 = {1160,350,30,150};
-  SDL_Rect separator = {600,0,5,950};
-  SDL_Rect ball = {600,350,20,20};
-
 
   if(SDL_Init(SDL_INIT_VIDEO) != 0){
     fprintf(stderr,"Error: %s",SDL_GetError());
     goto Quit;
   }
-
 
   window = SDL_CreateWindow("pong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1200,950,SDL_WINDOW_SHOWN);
 
@@ -25,7 +34,6 @@ int main(){
     fprintf(stderr, "Erreur: %s",SDL_GetError());
     goto Quit;
   }
-
 
   renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
   if(renderer == NULL){
@@ -42,14 +50,63 @@ int main(){
     goto Quit;
   }
 
-  SDL_SetRenderDrawColor(renderer,255,255,255,255);
-  SDL_RenderFillRect(renderer,&rect1);
-  SDL_RenderFillRect(renderer,&rect2);
-  SDL_RenderFillRect(renderer,&separator);
-  SDL_RenderFillRect(renderer,&ball);
 
+
+
+  // bool quit = false;
+
+  // int run = 1;
+//   SDL_Event event;
+//   // while(run == 1){
+//   while(quit){
+//
+//     while(SDL_PollEvent(&event)){
+//       if(event.type == SDL_QUIT){
+//
+//       }
+//
+//        if(event.type == SDL_KEYDOWN){
+//           switch(event.key.keysym.sym){
+//             case SDLK_UP:
+//             printf("Pressed up !");
+//                 rect2.y = rect2.y + 10;
+//                 break;
+//             case SDLK_DOWN:
+//                 printf("Pressed down!");
+//                 rect2.y = rect2.y - 10;
+//                 break;
+//             default:
+//                 break;
+//           }
+//       }
+//   }
+// }
+
+SDL_Event event;
+// while(run == 1){
+// while(quit){
+
+  while(SDL_PollEvent(&event)){
+      if(event.type == SDL_KEYDOWN){
+
+          switch(event.key.keysym.sym){
+            case SDLK_z:
+                printf("Pressed up !");
+                rect2.y += 10;
+                break;
+              break;
+          case SDLK_s:
+              printf("Pressed down!");
+              rect2.y -= 10;
+              break;
+          default:
+              break;
+        }
+    }
+  }
+
+  draw_rect();
   SDL_RenderPresent(renderer);
-
   SDL_Delay(4000);
 
   status = EXIT_SUCCESS;
@@ -61,3 +118,4 @@ int main(){
       SDL_Quit();
       return status;
 }
+
